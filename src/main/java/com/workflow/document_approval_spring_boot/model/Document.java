a4387bc67e62e61336cd40cab277f312bfe8fc4f
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-30 10:18:35 +0200

    entities-commit

--- src/main/java/com/workflow/document_approval_spring_boot/model/Document.java
+++ src/main/java/com/workflow/document_approval_spring_boot/model/Document.java
@@ -1,2 +1,29 @@
-package com.workflow.document_approval_spring_boot.model;public class Document {
+package com.workflow.document_approval_spring_boot.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Document {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private DocumentStatus status; // DRAFT, SUBMITTED, APPROVED, REJECTED
+
+    private Integer version;
+
+    @ManyToOne
+    private User creator;
+
+    private Integer currentStageIndex; // pointer to current stage
+
+    private LocalDate createdAt;
 }
--- src/main/java/com/workflow/document_approval_spring_boot/model/User.java
+++ src/main/java/com/workflow/document_approval_spring_boot/model/User.java
@@ -1,2 +1,21 @@
-package com.workflow.document_approval_spring_boot.model;public class User {
+package com.workflow.document_approval_spring_boot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password; // hashed
+    private String role; // e.g., "CLERK", "MANAGER", "DIRECTOR"
+    private String department;
+
+    private boolean active;
 }
+
