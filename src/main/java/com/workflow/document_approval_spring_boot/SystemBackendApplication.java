016fb69e08a9558ab88337c7f80cab4eb73aabed
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-30 10:13:20 +0200

    initial-commit

---
+++ src/main/java/com/workflow/document_approval_spring_boot/SystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.document_approval_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workflow/document_approval_spring_boot/SystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.document_approval_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
