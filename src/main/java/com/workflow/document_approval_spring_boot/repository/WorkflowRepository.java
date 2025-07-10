79a1c98490f0fb173130c2e00ba9e1497b0bd79a
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-10 22:46:38 +0200

    initial-approvalStageService-commit

--- src/main/java/com/workflow/document_approval_spring_boot/repository/WorkflowRepository.java
+++ src/main/java/com/workflow/document_approval_spring_boot/repository/WorkflowRepository.java
@@ -1,2 +1,9 @@
-package com.workflow.document_approval_spring_boot.repository;public interface WorkflowRepository {
+package com.workflow.document_approval_spring_boot.repository;
+
+import com.workflow.document_approval_spring_boot.model.Workflow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkflowRepository extends JpaRepository <Workflow, Long> {
 }
