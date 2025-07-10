a068f21836e3f7245a04c87da5ba4f7dc103a2e2
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-10 22:43:18 +0200

    second-repo-commit

--- src/main/java/com/workflow/document_approval_spring_boot/repository/ApprovalStageRepository.java
+++ src/main/java/com/workflow/document_approval_spring_boot/repository/ApprovalStageRepository.java
@@ -1,2 +1,9 @@
-package com.workflow.document_approval_spring_boot.repository;public interface ApprovalStageRepository {
+package com.workflow.document_approval_spring_boot.repository;
+
+import com.workflow.document_approval_spring_boot.model.ApprovalStage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApprovalStageRepository extends JpaRepository <ApprovalStage, Long> {
 }
