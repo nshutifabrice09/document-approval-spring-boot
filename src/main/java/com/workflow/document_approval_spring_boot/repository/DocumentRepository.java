774c8b5c735593d9878d2ffa293d5440c61dd394
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-10 22:45:22 +0200

    final-repo-commit

--- src/main/java/com/workflow/document_approval_spring_boot/repository/DocumentRepository.java
+++ src/main/java/com/workflow/document_approval_spring_boot/repository/DocumentRepository.java
@@ -1,2 +1,9 @@
-package com.workflow.document_approval_spring_boot.repository;public interface DocumentRepository {
+package com.workflow.document_approval_spring_boot.repository;
+
+import com.workflow.document_approval_spring_boot.model.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository <Document, Long> {
 }
--- src/main/java/com/workflow/document_approval_spring_boot/repository/UserRepository.java
+++ src/main/java/com/workflow/document_approval_spring_boot/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.workflow.document_approval_spring_boot.repository;public interface UserRepository {
+package com.workflow.document_approval_spring_boot.repository;
+
+import com.workflow.document_approval_spring_boot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
 }
