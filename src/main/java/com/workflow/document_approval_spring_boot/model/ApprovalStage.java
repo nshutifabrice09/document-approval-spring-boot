b56dd038925df83a2aa8f04d252de000e24a2115
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-16 09:52:00 +0200

    update-model-commit

--- src/main/java/com/workflow/document_approval_spring_boot/model/ApprovalStage.java
+++ src/main/java/com/workflow/document_approval_spring_boot/model/ApprovalStage.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
@@ -32,4 +33,83 @@ public class ApprovalStage {
     @OneToMany(mappedBy = "stage")
     private List<ApprovalAction> approvalActions;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStageName() {
+        return stageName;
+    }
+
+    public void setStageName(String stageName) {
+        this.stageName = stageName;
+    }
+
+    public int getStageOrder() {
+        return stageOrder;
+    }
+
+    public void setStageOrder(int stageOrder) {
+        this.stageOrder = stageOrder;
+    }
+
+    public String getRequiredRole() {
+        return requiredRole;
+    }
+
+    public void setRequiredRole(String requiredRole) {
+        this.requiredRole = requiredRole;
+    }
+
+    public String getApproverType() {
+        return approverType;
+    }
+
+    public void setApproverType(String approverType) {
+        this.approverType = approverType;
+    }
+
+    public int getDeadlineInDays() {
+        return deadlineInDays;
+    }
+
+    public void setDeadlineInDays(int deadlineInDays) {
+        this.deadlineInDays = deadlineInDays;
+    }
+
+    public boolean isAutoApproveAfterDeadline() {
+        return autoApproveAfterDeadline;
+    }
+
+    public void setAutoApproveAfterDeadline(boolean autoApproveAfterDeadline) {
+        this.autoApproveAfterDeadline = autoApproveAfterDeadline;
+    }
+
+    public String getEscalationRule() {
+        return escalationRule;
+    }
+
+    public void setEscalationRule(String escalationRule) {
+        this.escalationRule = escalationRule;
+    }
+
+    public Workflow getWorkflow() {
+        return workflow;
+    }
+
+    public void setWorkflow(Workflow workflow) {
+        this.workflow = workflow;
+    }
+
+    public List<ApprovalAction> getApprovalActions() {
+        return approvalActions;
+    }
+
+    public void setApprovalActions(List<ApprovalAction> approvalActions) {
+        this.approvalActions = approvalActions;
+    }
 }
--- src/main/java/com/workflow/document_approval_spring_boot/model/Comment.java
+++ src/main/java/com/workflow/document_approval_spring_boot/model/Comment.java
@@ -1,2 +1,80 @@
-package com.workflow.document_approval_spring_boot.model;public class Comment {
+package com.workflow.document_approval_spring_boot.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "comments")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private LocalDate timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "document_id")
+    private Document document;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDate timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Comment getParentComment() {
+        return parentComment;
+    }
+
+    public void setParentComment(Comment parentComment) {
+        this.parentComment = parentComment;
+    }
 }
