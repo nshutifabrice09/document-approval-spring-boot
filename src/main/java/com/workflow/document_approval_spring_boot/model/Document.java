5df1c791549acc9f63b2cbca3991eeeedaedc2a9
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-16 10:00:50 +0200

    repository-update-commit

--- src/main/java/com/workflow/document_approval_spring_boot/model/Document.java
+++ src/main/java/com/workflow/document_approval_spring_boot/model/Document.java
@@ -56,4 +56,164 @@ public class Document {
 
     @OneToMany(mappedBy = "document")
     private List<DeadlineExtensionRequest> deadlineRequests;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getConfidentialityLevel() {
+        return confidentialityLevel;
+    }
+
+    public void setConfidentialityLevel(String confidentialityLevel) {
+        this.confidentialityLevel = confidentialityLevel;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<ApprovalAction> getApprovalActions() {
+        return approvalActions;
+    }
+
+    public void setApprovalActions(List<ApprovalAction> approvalActions) {
+        this.approvalActions = approvalActions;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<Attachment> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(List<Attachment> attachments) {
+        this.attachments = attachments;
+    }
+
+    public List<ChangeRequest> getChangeRequests() {
+        return changeRequests;
+    }
+
+    public void setChangeRequests(List<ChangeRequest> changeRequests) {
+        this.changeRequests = changeRequests;
+    }
+
+    public List<CustomField> getCustomFields() {
+        return customFields;
+    }
+
+    public void setCustomFields(List<CustomField> customFields) {
+        this.customFields = customFields;
+    }
+
+    public List<DeadlineExtensionRequest> getDeadlineRequests() {
+        return deadlineRequests;
+    }
+
+    public void setDeadlineRequests(List<DeadlineExtensionRequest> deadlineRequests) {
+        this.deadlineRequests = deadlineRequests;
+    }
 }
--- src/main/java/com/workflow/document_approval_spring_boot/model/Task.java
+++ src/main/java/com/workflow/document_approval_spring_boot/model/Task.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Entity
-@Data
+@Table(name = "tasks")
 @NoArgsConstructor
 @AllArgsConstructor
 
