9193be7340ee62d5cf27267c3d8c9a493f447408
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-16 16:43:48 +0200

    constructorParameters-approvalActionService-commit

--- src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalActionServiceImplementation.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalActionServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.workflow.document_approval_spring_boot.service;public class ApprovalActionServiceImplementation {
+package com.workflow.document_approval_spring_boot.service;
+
+import com.workflow.document_approval_spring_boot.model.ApprovalAction;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ApprovalActionServiceImplementation implements ApprovalActionService{
+    @Override
+    public List<ApprovalAction> approvalActionsList() {
+        return null;
+    }
+
+    @Override
+    public ApprovalAction getApprovalActionById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ApprovalAction saveApprovalAction(ApprovalAction approvalAction, Long documentId, Long stageId, Long actionByUserId) {
+        return null;
+    }
+
+    @Override
+    public ApprovalAction updateApprovalAction(Long id, ApprovalAction approvalAction) {
+        return null;
+    }
+
+    @Override
+    public void removeApprovalAction(Long id) {
+
+    }
 }
