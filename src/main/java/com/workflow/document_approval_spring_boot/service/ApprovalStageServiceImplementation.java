7d931a957aa1192896e908dfbd9121d20702f539
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-16 15:38:37 +0200

    parameters-approvalStageServiceImpl-commit

--- src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalStageServiceImplementation.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalStageServiceImplementation.java
@@ -1,12 +1,23 @@
 package com.workflow.document_approval_spring_boot.service;
 
 import com.workflow.document_approval_spring_boot.model.ApprovalStage;
+import com.workflow.document_approval_spring_boot.repository.ApprovalStageRepository;
+import com.workflow.document_approval_spring_boot.repository.WorkflowRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ApprovalStageServiceImplementation implements ApprovalStageService{
+
+    private final ApprovalStageRepository approvalStageRepository;
+    private final WorkflowRepository workflowRepository;
+
+    public ApprovalStageServiceImplementation(ApprovalStageRepository approvalStageRepository, WorkflowRepository workflowRepository) {
+        this.approvalStageRepository = approvalStageRepository;
+        this.workflowRepository = workflowRepository;
+    }
+
     @Override
     public List<ApprovalStage> approvalStageList() {
         return null;
