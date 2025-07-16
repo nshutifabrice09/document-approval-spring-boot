5033e0ef9b75a323163a2f583a7fa15c52a7c9fc
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-16 15:22:41 +0200

    second-approvalStageServiceImpl-commit

--- src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalStageServiceImplementation.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalStageServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.workflow.document_approval_spring_boot.service;
 
+import com.workflow.document_approval_spring_boot.model.ApprovalStage;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ApprovalStageServiceImplementation implements ApprovalStageService{
+    @Override
+    public List<ApprovalStage> approvalStageList() {
+        return null;
+    }
+
+    @Override
+    public ApprovalStage getApprovalStageById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ApprovalStage saveApprovalStage(ApprovalStage approvalStage, Long workflowId) {
+        return null;
+    }
+
+    @Override
+    public ApprovalStage updateApprovalStage(Long id, ApprovalStage approvalStage) {
+        return null;
+    }
+
+    @Override
+    public void removeApprovalStage(Long id) {
+
+    }
 }
