5e1dd9ced023cf5de575f2804657140ebb315793
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-16 09:30:28 +0200

    update-model-commit

--- src/main/java/com/workflow/document_approval_spring_boot/model/User.java
+++ src/main/java/com/workflow/document_approval_spring_boot/model/User.java
@@ -25,5 +25,61 @@ public class User {
     private String department;
 
     private boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
 
--- src/main/java/com/workflow/document_approval_spring_boot/model/Workflow.java
+++ src/main/java/com/workflow/document_approval_spring_boot/model/Workflow.java
@@ -22,4 +22,36 @@ public class Workflow {
 
     @OneToMany(mappedBy = "workflow", cascade = CascadeType.ALL)
     private List<ApprovalStageTemplate> stages;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<ApprovalStageTemplate> getStages() {
+        return stages;
+    }
+
+    public void setStages(List<ApprovalStageTemplate> stages) {
+        this.stages = stages;
+    }
 }
