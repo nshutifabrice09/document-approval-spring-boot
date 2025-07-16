d475c074c6296f98df5024fc4289ab344f66ac36
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-16 10:59:59 +0200

    repository-update-commit

--- src/main/java/com/workflow/document_approval_spring_boot/repository/CommentRepository.java
+++ src/main/java/com/workflow/document_approval_spring_boot/repository/CommentRepository.java
@@ -1,2 +1,9 @@
-package com.workflow.document_approval_spring_boot.repository;public interface CommentRepository {
+package com.workflow.document_approval_spring_boot.repository;
+
+import com.workflow.document_approval_spring_boot.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository <Comment, Long> {
 }
