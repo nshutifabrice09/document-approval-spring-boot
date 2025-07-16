a3c19dc8e9148c8fc4c2e6c747d717a5f3ccb1c3
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-16 10:55:10 +0200

    repository-update-commit

--- src/main/java/com/workflow/document_approval_spring_boot/repository/ChangeRequestRepository.java
+++ src/main/java/com/workflow/document_approval_spring_boot/repository/ChangeRequestRepository.java
@@ -1,2 +1,9 @@
-package com.workflow.document_approval_spring_boot.repository;public interface ChangeRequestRepository {
+package com.workflow.document_approval_spring_boot.repository;
+
+import com.workflow.document_approval_spring_boot.model.ChangeRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChangeRequestRepository extends JpaRepository<ChangeRequest, Long> {
 }
--- src/main/java/com/workflow/document_approval_spring_boot/repository/CustomFieldRepository.java
+++ src/main/java/com/workflow/document_approval_spring_boot/repository/CustomFieldRepository.java
@@ -1,2 +1,9 @@
-package com.workflow.document_approval_spring_boot.repository;public interface CustomFieldRepository {
+package com.workflow.document_approval_spring_boot.repository;
+
+import com.workflow.document_approval_spring_boot.model.CustomField;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomFieldRepository extends JpaRepository<CustomField, Long> {
 }
