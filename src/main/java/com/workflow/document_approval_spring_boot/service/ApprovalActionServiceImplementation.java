d78c0820510d73e20effb96b9f312f1c51c3bc2b
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-17 15:55:56 +0200

    repo-update-cpmmit

--- src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalActionServiceImplementation.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalActionServiceImplementation.java
@@ -1,12 +1,29 @@
 package com.workflow.document_approval_spring_boot.service;
 
 import com.workflow.document_approval_spring_boot.model.ApprovalAction;
+import com.workflow.document_approval_spring_boot.repository.ApprovalActionRepository;
+import com.workflow.document_approval_spring_boot.repository.ApprovalStageRepository;
+import com.workflow.document_approval_spring_boot.repository.DocumentRepository;
+import com.workflow.document_approval_spring_boot.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ApprovalActionServiceImplementation implements ApprovalActionService{
+
+    private final ApprovalActionRepository approvalActionRepository;
+    private final DocumentRepository documentRepository;
+    private final ApprovalStageRepository approvalStageRepository;
+    private final UserRepository userRepository;
+
+    public ApprovalActionServiceImplementation(ApprovalActionRepository approvalActionRepository, DocumentRepository documentRepository, ApprovalStageRepository approvalStageRepository, UserRepository userRepository) {
+        this.approvalActionRepository = approvalActionRepository;
+        this.documentRepository = documentRepository;
+        this.approvalStageRepository = approvalStageRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<ApprovalAction> approvalActionsList() {
         return null;
