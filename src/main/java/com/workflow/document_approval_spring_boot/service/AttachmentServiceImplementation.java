079dfbb0b743fe2e9fee20a8730958c6c56812ac
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-18 22:58:00 +0200

    updates-attachmentServiceImpl-commit

--- src/main/java/com/workflow/document_approval_spring_boot/service/AttachmentServiceImplementation.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/AttachmentServiceImplementation.java
@@ -1,8 +1,51 @@
 package com.workflow.document_approval_spring_boot.service;
 
+import com.workflow.document_approval_spring_boot.model.Attachment;
+import com.workflow.document_approval_spring_boot.repository.AttachmentRepository;
+import com.workflow.document_approval_spring_boot.repository.DocumentRepository;
+import com.workflow.document_approval_spring_boot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AttachmentServiceImplementation implements AttachmentService{
 
+    private final AttachmentRepository attachmentRepository;
+    private final DocumentRepository documentRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AttachmentServiceImplementation(AttachmentRepository attachmentRepository, DocumentRepository documentRepository, UserRepository userRepository) {
+        this.attachmentRepository = attachmentRepository;
+        this.documentRepository = documentRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public List<Attachment> attachmentList() {
+        return null;
+    }
+
+    @Override
+    public Attachment getAttachmentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Attachment saveAttachment(Attachment attachment, Long documentId, Long uploadedBy) {
+        return null;
+    }
+
+    @Override
+    public Attachment updateAttachment(Long id, Attachment attachment) {
+        return null;
+    }
+
+    @Override
+    public void removeAttachment(Long id) {
+
+    }
 }
