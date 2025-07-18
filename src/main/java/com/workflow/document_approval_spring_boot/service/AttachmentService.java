a9efc2997a3c235a57ab5d1b2aeb2640024279d1
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-18 22:55:36 +0200

    final-attachmentService-commit

--- src/main/java/com/workflow/document_approval_spring_boot/service/AttachmentService.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/AttachmentService.java
@@ -1,4 +1,14 @@
 package com.workflow.document_approval_spring_boot.service;
 
+
+import com.workflow.document_approval_spring_boot.model.Attachment;
+
+import java.util.List;
+
 public interface AttachmentService {
+    List<Attachment> attachmentList();
+    Attachment getAttachmentById(Long id);
+    Attachment saveAttachment(Attachment attachment, Long documentId, Long uploadedBy);
+    Attachment updateAttachment(Long id, Attachment attachment);
+    void removeAttachment (Long id);
 }
