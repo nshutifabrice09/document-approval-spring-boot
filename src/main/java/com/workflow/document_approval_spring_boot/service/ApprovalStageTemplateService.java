462715cf2abf2d5155773976b40577f866074b4f
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-18 22:20:46 +0200

    initial-approvalStageTemplateServiceImpl-commit

--- src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalStageTemplateService.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/ApprovalStageTemplateService.java
@@ -6,5 +6,9 @@ import java.util.List;
 
 public interface ApprovalStageTemplateService {
     List<ApprovalStageTemplate> approvalStageTemplateList();
+    ApprovalStageTemplate getApprovalStageTemplateById(Long id);
+    ApprovalStageTemplate saveApprovalStageTemplate(ApprovalStageTemplate approvalStageTemplate, Long workflowId);
+    ApprovalStageTemplate updateApprovalStageTemplate(Long id, ApprovalStageTemplate approvalStageTemplate);
+    void removeApprovalStageTemplate(Long id);
 
 }
