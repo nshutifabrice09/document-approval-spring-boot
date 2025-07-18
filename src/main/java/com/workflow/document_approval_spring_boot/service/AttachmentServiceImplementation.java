816da2954c77ae044c4bce6dbe88752d3368a54b
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-18 22:56:34 +0200

    initial-attachmentServiceImpl-commit

---
+++ src/main/java/com/workflow/document_approval_spring_boot/service/AttachmentServiceImplementation.java
@@ -0,0 +1,8 @@
+package com.workflow.document_approval_spring_boot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AttachmentServiceImplementation implements AttachmentService{
+
+}
