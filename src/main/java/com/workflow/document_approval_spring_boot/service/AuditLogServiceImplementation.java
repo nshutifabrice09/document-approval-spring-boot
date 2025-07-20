14ee7192974d5a385c9e3be505adc5f5d92c62f8
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-20 18:46:41 +0200

    update-auditLogServiceImp-commit

--- src/main/java/com/workflow/document_approval_spring_boot/service/AuditLogServiceImplementation.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/AuditLogServiceImplementation.java
@@ -1,12 +1,24 @@
 package com.workflow.document_approval_spring_boot.service;
 
 import com.workflow.document_approval_spring_boot.model.AuditLog;
+import com.workflow.document_approval_spring_boot.repository.AuditLogRepository;
+import com.workflow.document_approval_spring_boot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AuditLogServiceImplementation implements AuditLogService{
+    private final AuditLogRepository auditLogRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuditLogServiceImplementation(AuditLogRepository auditLogRepository, UserRepository userRepository) {
+        this.auditLogRepository = auditLogRepository;
+        this.userRepository = userRepository;
+    }
+    
     @Override
     public List<AuditLog> auditLogList() {
         return null;
