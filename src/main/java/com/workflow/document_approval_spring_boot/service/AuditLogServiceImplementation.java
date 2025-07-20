da865728038e154a53615ef00bec87bdbc2aa06e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-07-20 18:51:42 +0200

    final-auditLogServiceImp-commit

--- src/main/java/com/workflow/document_approval_spring_boot/service/AuditLogServiceImplementation.java
+++ src/main/java/com/workflow/document_approval_spring_boot/service/AuditLogServiceImplementation.java
@@ -1,6 +1,7 @@
 package com.workflow.document_approval_spring_boot.service;
 
 import com.workflow.document_approval_spring_boot.model.AuditLog;
+import com.workflow.document_approval_spring_boot.model.User;
 import com.workflow.document_approval_spring_boot.repository.AuditLogRepository;
 import com.workflow.document_approval_spring_boot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,19 +29,29 @@ public class AuditLogServiceImplementation implements AuditLogService{
     public AuditLog getAuditLogById(Long id) {
         return auditLogRepository.findAuditLogById(id);
     }
-
     @Override
     public AuditLog saveAuditLog(AuditLog auditLog, Long performedBy) {
-        return null;
+        User user = userRepository.findUserById(performedBy);
+        auditLog.setPerformedBy(user);
+        return auditLogRepository.save(auditLog);
     }
 
     @Override
     public AuditLog updateAuditLog(Long id, AuditLog auditLog) {
+        AuditLog existAuditLog = auditLogRepository.findAuditLogById(id);
+        if(existAuditLog != null){
+            existAuditLog.setEntityType(auditLog.getEntityType());
+            existAuditLog.setEntityId(auditLog.getEntityId());
+            existAuditLog.setAction(auditLog.getAction());
+            existAuditLog.setTimestamp(auditLog.getTimestamp());
+            existAuditLog.setDetails(auditLog.getDetails());
+            return auditLogRepository.save(existAuditLog);
+        }
         return null;
     }
 
     @Override
     public void removeAuditLog(Long id) {
-
+        auditLogRepository.deleteById(id);
     }
 }
